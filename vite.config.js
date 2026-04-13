d075dddf3227eed3d0af03629822db33ed2b1992
MatheusTeixeiraGaldino <departamentopessoal@otaviolage.com>
2026-04-12 23:02:30 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
+  base: '/quiz-rh/', // Adicione esta linha com o nome exato do seu repositório
   build: {
     outDir: 'dist',
     sourcemap: false,
